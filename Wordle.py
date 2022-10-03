55f6e07ab1acbc8fff28213a34fceceb15244749
TannerGlazier <tannerglazier@gmail.com>
2022-10-03 13:29:35 -0600

    Milestone 2

---
+++ Wordle.py
@@ -0,0 +1,36 @@
+# File: Wordle.py
+
+"""
+This module is the starter file for the Wordle assignment.
+BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
+"""
+
+import random
+
+from WordleDictionary import FIVE_LETTER_WORDS
+from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+
+def wordle():
+
+    def enter_action(s):
+        gw.show_message("You have to implement this method.")
+
+    gw = WordleGWindow()
+    gw.add_enter_listener(enter_action)
+
+    random_word = random.choice(FIVE_LETTER_WORDS).upper()
+    print(random_word)
+
+    for i, letter in enumerate(random_word):
+        gw.set_square_letter(0, i, letter)
+
+    def enter_action(s): # Milestone 2 function
+        s = s.lower()
+        if s in wordList:
+            gw.show_message("This is a valid guess")
+        else:
+            gw.show_message("Not in Word List")
+    wordList = set(FIVE_LETTER_WORDS)
+
+if __name__ == "__main__":
+    wordle()
